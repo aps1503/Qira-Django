b10e7d37c1192a990fc738ee93e667694d4aedd2
Lainkai <tefman7422@gmail.com>
2019-12-12 02:30:08 -0500

    Something to please the masses

--- robot_control/views.py
+++ robot_control/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Congrats! You are now viewing QIRA 0.0.-1", content_type='text/plain')
