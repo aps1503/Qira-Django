58d4d4abe60bd3a03797ef41e3e487d245288552
Jake Armstrong <Lainkai@users.noreply.github.com>
2019-12-12 16:56:53 -0500

    First Official Version

--- robot_control/views.py
+++ robot_control/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Congrats! You are now viewing QIRA 0.0.-1", content_type='text/plain')
+    return HttpResponse("Congrats! You are now viewing QIRA 0.0.1", content_type='text/plain')
