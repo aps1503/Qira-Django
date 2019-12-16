7d1b720810083c8b9cd5654157de19c7fadfbebc
Jake Armstrong <Lainkai@users.noreply.github.com>
2019-12-16 00:02:18 -0500

    Getting QIRA Version

--- qira_interface/views.py
+++ qira_interface/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
 import os
 
 # Create your views here.
 def index(request):
     context = {
-        'qv': os.getenv('QIRA-VERSION'),
+        'qv': settings.VERSION,
     }
     return render(request,'qira_interface/index.html',context)
