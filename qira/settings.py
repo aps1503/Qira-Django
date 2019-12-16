fefc98a3ca70b1bcf98e936f0039d73ee7b787bd
Jake Armstrong <Lainkai@users.noreply.github.com>
2019-12-15 22:44:33 -0500

    Qira Face Looks Cool
    Not really the face, but the loading page.

--- qira/settings.py
+++ qira/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'robot_control.apps.RobotControlConfig'
+    'robot_control.apps.RobotControlConfig',
+    'qira_interface.apps.QiraInterfaceConfig'
 ]
 
 MIDDLEWARE = [
--- qira_interface/views.py
+++ qira_interface/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import os
 
 # Create your views here.
 def index(request):
-    pass
+    context = {
+        'qv': os.getenv('QIRA-VERSION'),
+    }
+    return render(request,'qira_interface/index.html',context)
